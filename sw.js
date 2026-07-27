b2cbb2c84221263322e1c569eabd45feb5dcdaa6
syakiela666-lang <syakiela666@gmail.com>
2026-07-27 18:48:48 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -18,9 +18,15 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    fetch(event.request).then(response => {
+      // Update cache
+      if(response && response.status === 200 && response.type === 'basic') {
+        let responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone));
+      }
+      return response;
+    }).catch(() => {
+      return caches.match(event.request);
+    })
   );
 });
